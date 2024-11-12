de539dc936752c070e668fe58dc63607114c6690
Andrei Hileuski <63409364+Uki-coder@users.noreply.github.com>
2024-11-12 15:41:27 +0100

    project done

--- E1_py/main.py
+++ E1_py/main.py
@@ -1,8 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
+
 import read_file
 from matplotlib import cm
-from matplotlib import colormaps
+
+def makeplot(position,angle):
+    fig, ax = plt.subplots(subplot_kw={'projection': '3d'})
+    surf = ax.plot_surface(y, x, phi, cmap = cm.hsv)
+    fig.colorbar(surf, shrink=0.5, aspect=5, label=r'rozkład $\varphi$ w obszarze $XY$')
+    ax.set_xlabel(r'$Y$')
+    ax.set_ylabel(r'$X$')
+    ax.set_zlabel(r'$\varphi[V]$')
+    ax.view_init(position, angle)
+    return ax
 
 x = read_file.read_line('x_collected.txt')
 y = read_file.read_row('y_collected.txt')
@@ -15,20 +26,22 @@ plus_electrode = plt.Circle((5, 8.5), 3.75, color='r', fill=False)
 
 
 plt.rcParams['text.usetex'] = True
-'''
+
 fig, ax = plt.subplots()
-plt.quiver(x,y, e_x, e_y)
+
 ax.add_patch(plus_electrode)
 fig.set_size_inches(26,9)
-'''
+x_min, y_min = [26,26],[4.5,11.5]
+plt.axvline(26.5,0.2,0.75,color='black', linewidth = 4) #minus electrod
+plt.legend(['katoda (cylinder)', 'anoda (płyta)'])
+e_plot = plt.quiver(x,y, e_x, e_y,width = 0.001, color = 'blue', headlength = 7)
+ax.set_xlabel('$X$')
+ax.set_ylabel('$Y$')
+ax.quiverkey(e_plot, 0.925,1.05,0.2, label=r'rozkład $\overrightarrow{E}$ w obsszarze $XY$',labelpos='E')
 
-plt.rcParams['text.usetex'] = True
-fig, ax = plt.subplots(subplot_kw={'projection': '3d'})
-surf = ax.plot_surface(y, x, phi, cmap = cm.twilight_shifted)
-ax.set_xlabel(r'$Y$')
-ax.set_ylabel(r'$X$')
-ax.set_zlabel(r'$\varphi[V]$')
-#ax.view_init(20, 0)
-ax.view_init(30,30)
+
+
+makeplot(20,0)
+makeplot(30,30)
 plt.show()
 
